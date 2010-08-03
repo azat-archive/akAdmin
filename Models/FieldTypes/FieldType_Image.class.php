09b7ed050a4a724c3e6414e574ce0c5d0d2fe87d
Azat Khuzhin <dohardgopro@gmail.com>
2010-08-03 21:04:50 +0000

    Writen FieldType for Images Delete / Update / And generation previews for every image

--- Models/FieldTypes/FieldType_Image.class.php
+++ Models/FieldTypes/FieldType_Image.class.php
@@ -3,12 +3,45 @@
 /**
  * Image FieldType
  * 
+ * @TODO add normal links for big img
+ * 
  * @author Azat Khuzhin <dohardgopro@gmail.com>
  * @package akAdmin
  * @licence GPLv2
  */
 
 class FieldType_Image extends FieldTypes {
+	/**
+	 * Path under DOCUMENT_ROOT
+	 * 
+	 * @var string
+	 */
+	static $path = '/tmp/';
+	/**
+	 * Path to image previews under DOCUMENT_ROOT
+	 * 
+	 * @var string
+	 */
+	static $previewPath = '/tmp/previews/';
+	/**
+	 * Preview quality
+	 * Do not edit this if your don`t understand for what this!
+	 * (After your edit this property old previes is not shows, and not deleted old previews!)
+	 * 
+	 * @var int
+	 */
+	static $previewQuality = 90;
+	/**
+	 * Preview sizes
+	 * Do not delete first element of this array if your don`t understand for what this!
+	 * (After your delete first element of this array old previes is not shows, and not deleted old previews!)
+	 * 
+	 * @var array
+	 */
+	static $previewSizes = array('100x100');
+	/**
+	 * @see parent::$DBTypes
+	 */
 	public $DBTypes = array('text', 'varchar', 'char');
 
 	static function getInstance() {
@@ -17,22 +50,99 @@ class FieldType_Image extends FieldTypes {
 		return $object;
 	}
 
+	/**
+	 * @see parent::get()
+	 */
 	public function get($value = null) {
-		return $value;
-	}
-
-	public function getForEdit($field = null, $value = null, $fieldTransltaion = null) {
+		if (!$value || !isset(self::$previewSizes[0])) return null;
+		
+		preg_match('@(\d+?)x(\d+?)@Uis', self::$previewSizes[0], $size);
 		return sprintf(
-			'<label><strong>%s</strong><input type="text" name="%s" value="%s" /></label>',
-			$fieldTransltaion, $field, $this->get($value)
+			'<a href="%s%s"><img src="%s%u_%u_%u_%s" alt="" /></a>',
+			self::$path, $value, self::$previewPath, $size[1], $size[2], self::$previewQuality, $value
 		);
 	}
 
+	/**
+	 * @see parent::getForEdit()
+	 */
+	public function getForEdit($field = null, $value = null, $fieldTransltaion = null) {
+		return sprintf(
+			'<label><strong>%s</strong><input type="file" name="%s" /></label>' .
+			'<input type="checkbox" name="%s[delete]" title="Will be deleted automaticly if new is uploaded" />Delete old?' .
+			'%s' .
+			'<input type="hidden" name="%s[old]" value="%s" />',
+			$fieldTransltaion, $field,
+			$field, $this->get($value),
+			$field, $value
+		);
+	}
+
+	/**
+	 * @see parent::getForSearch()
+	 */
 	public function getForSearch($field = null, $value = null) {
 		throw new akException('WRITE THIS PART');
 	}
 
+	/**
+	 * @see parent::set()
+	 * 
+	 * @throws akException
+	 * 				if error occured while deleting image or uploading new image
+	 * 				but not if error occured file creating or deleting preview for image
+	 */
 	public function set($value = null) {
-		return $value;
+		// old file (to delete)
+		$oldFile = ($value['old'] ? sprintf('%s/%s', realpath(dirRoot . self::$path), $value['old']) : null);
+		// new file (from $_FILES array)
+		$newFile = (isset($value['file']) ? $value['file'] : null);
+		// is need to delete old file?
+		$delete = isset($value['delete']);
+		// new files is the old file, while new file is not uploaded
+		$newFileName = $value['old'];
+		// preview sizes
+		preg_match_all('@(\d+?)x(\d+?)(?:,|$)@Uis', join(',', self::$previewSizes), $previewSizes, PREG_SET_ORDER);
+		
+		
+		// delete old file, if we select checkbox "delete" or new file is uploaded
+		if ($delete || ($newFile && $newFile['error'] != 4)) {
+			if ($oldFile && file_exists($oldFile)) {
+				if (!unlink($oldFile)) {
+					throw new akException('Error occured while deleting old file.');
+				}
+				// delete all previews
+				foreach ($previewSizes as $size) {
+					unlink(sprintf('%s/%u_%u_%u_%s', realpath(dirRoot . self::$previewPath), $size[1], $size[2], self::$previewQuality, $newFileName));
+				}
+			}
+			$newFileName = null;
+		}
+		// copy new file
+		if ($newFile && $newFile['error'] != 4) {
+			$ext = akExec::getFileExt($newFile['name']);
+			$newFileName = sprintf('%s.%s', randomStr(10), $ext);
+			while (file_exists(sprintf('%s/%s', realpath(dirRoot . self::$path), $newFileName))) {
+				$newFileName = sprintf('%s.%s', randomStr(10), $ext);
+			}
+			
+			if (!copy($newFile['tmp_name'], sprintf('%s/%s', realpath(dirRoot . self::$path), $newFileName))) {
+				throw new akException('Error occured while copy new file.');
+			}
+			
+			// generate previews
+			$image = akImage::getInstance(sprintf('%s/%s', realpath(dirRoot . self::$path), $newFileName));
+			foreach ($previewSizes as $size) {
+				$image->resize(
+					$size[1], $size[2],
+					sprintf('%s/%u_%u_%u_%s', realpath(dirRoot . self::$previewPath), $size[1], $size[2], self::$previewQuality, $newFileName),
+					self::$previewQuality,
+					akImage::resizeByXY,
+					$zoom = false
+				);
+			}
+		}
+		
+		return $newFileName;
 	}
 }
