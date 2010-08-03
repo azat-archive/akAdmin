5cb20296a9bea9e84b5bf27e59cce5f8b3c8a3d1
Azat Khuzhin <dohardgopro@gmail.com>
2010-08-03 19:58:55 +0000

    Write FieldType for Files Upload / Delete / Update is allowed

--- Models/FieldTypes/FieldType_File.class.php
+++ Models/FieldTypes/FieldType_File.class.php
@@ -14,7 +14,7 @@ class FieldType_File extends FieldTypes {
 	 * 
 	 * @var string
 	 */
-	static $path = '/';
+	static $path = '/tmp/';
 	public $DBTypes = array('text', 'varchar', 'char');
 
 	static function getInstance() {
@@ -24,13 +24,18 @@ class FieldType_File extends FieldTypes {
 	}
 
 	public function get($value = null) {
-		return $value;
+		if (!$value) return null;
+		return sprintf('<a href="%s%s">%s</a>', self::$path, $value, $value);
 	}
 
 	public function getForEdit($field = null, $value = null, $fieldTransltaion = null) {
 		return sprintf(
-			'<label><strong>%s</strong><input type="text" name="%s" value="%s" /></label>',
-			$fieldTransltaion, $field, $this->get($value)
+			'<label><strong>%s</strong><input type="file" name="%s" /></label>' .
+			'<input type="checkbox" name="%s[delete]" title="Will be deleted automaticly if new is uploaded" />Delete %s?' .
+			'<input type="hidden" name="%s[old]" value="%s" />',
+			$fieldTransltaion, $field,
+			$field, ($value ? sprintf('<a href="%s%s">old</a>', self::$path, $value) : 'old'),
+			$field, $value
 		);
 	}
 
@@ -39,6 +44,36 @@ class FieldType_File extends FieldTypes {
 	}
 
 	public function set($value = null) {
-		return $value;
+		// old file (to delete)
+		$oldFile = ($value['old'] ? sprintf('%s/%s', realpath(dirRoot . self::$path), $value['old']) : null);
+		// new file (from $_FILES array)
+		$newFile = (isset($value['file']) ? $value['file'] : null);
+		// is need to delete old file?
+		$delete = isset($value['delete']);
+		// new files is the old file, while new file is not uploaded
+		$newFileName = $value['old'];
+		
+		// delete old file, if we select checkbox "delete" or new file is uploaded
+		if ($delete || ($newFile && $newFile['error'] != 4)) {
+			if ($oldFile && file_exists($oldFile)) {
+				if (!unlink($oldFile)) {
+					throw new akException('Error occured while deleting old file.');
+				}
+			}
+			$newFileName = null;
+		}
+		// copy new file
+		if ($newFile && $newFile['error'] != 4) {
+			$newFileName = randomStr(10);
+			while (file_exists(sprintf('%s/%s', realpath(dirRoot . self::$path), $newFileName))) {
+				$newFileName = randomStr(10);
+			}
+			
+			if (!copy($newFile['tmp_name'], sprintf('%s/%s', realpath(dirRoot . self::$path), $newFileName))) {
+				throw new akException('Error occured while copy new file.');
+			}
+		}
+		
+		return $newFileName;
 	}
 }
