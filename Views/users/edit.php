15d33918489ff034b797074af89966ca0019b272
Azat Khuzhin <dohardgopro@gmail.com>
2010-09-10 22:23:17 +0000

    Fix syntax error

--- Views/users/edit.php
+++ Views/users/edit.php
@@ -6,7 +6,7 @@
 		<li><label><strong>Email</strong><input type="text" name="email" value="<?=gfs((isset($_POST['email']) ? $_POST['email'] : null), $userInfo['email'])?>" /></label></li>
 		
 		<?if ($user['isAdmin']) {?>
-		<li><label><strong>Admin</strong><input type="checkbox" name="isAdmin"<?=(gfs((isset($_POST['isAdmin']) ? : null), $userInfo['isAdmin']) ? ' checked' : false)?> /></label></li>
+		<li><label><strong>Admin</strong><input type="checkbox" name="isAdmin"<?=(gfs((isset($_POST['isAdmin']) ? $_POST['isAdmin'] : null), $userInfo['isAdmin']) ? ' checked' : false)?> /></label></li>
 		<?}?>
 		
 		<li><input type="submit" value="Submit" /></li>
